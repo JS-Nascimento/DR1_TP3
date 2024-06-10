96e4a5e37d96a51d044598cf7a4f0e91f4d86a07
Jorge Nascimento <jorge@jstec.dev.br>
2024-06-10 20:07:33 -0300

    Initial commit

---
+++ src/main/java/br/edu/infnet/dr1_tp3/Dr1Tp3Application.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dr1_tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr1Tp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dr1Tp3Application.class, args);
+    }
+
+}
